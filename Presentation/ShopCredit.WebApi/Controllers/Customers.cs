df3b850660d18f107ace928d34cc837f3ba2a24d
tolunayz <102868718+tolunayz@users.noreply.github.com>
2024-07-23 16:49:11 +0300

    Api

---
+++ Presentation/ShopCredit.WebApi/Controllers/Customers.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopCredit.Application.CQRS.Commands;
+using ShopCredit.Application.CQRS.Handlers;
+using ShopCredit.Application.CQRS.Queries.CustomerQueries;
+
+namespace ShopCredit.WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class Customers : ControllerBase
+    {
+        private readonly CreateCustomerCommandHandler _createCustomerCommandHandler;
+        private readonly GetCustomerByIdQueryHandler _getCustomerByIdQueryHandler;
+        private readonly GetCustomerQueryHandler _getCustomerQueryHandler;
+        private readonly UpdateCustomerCommandHandler _updateCustomerCommandHandler;
+        private readonly RemoveCustomerCommandHandler _removeCustomerCommandHandler;
+
+        public Customers
+            (CreateCustomerCommandHandler createCustomerCommandHandler,
+            GetCustomerByIdQueryHandler getCustomerByIdQueryHandler,
+            GetCustomerQueryHandler getCustomerQueryHandler,
+            UpdateCustomerCommandHandler updateCustomerCommandHandler, 
+            RemoveCustomerCommandHandler removeCustomerCommandHandler
+            )
+        {
+            this._createCustomerCommandHandler = createCustomerCommandHandler;
+            this._getCustomerByIdQueryHandler = getCustomerByIdQueryHandler;
+            this._getCustomerQueryHandler = getCustomerQueryHandler;
+            this._updateCustomerCommandHandler = updateCustomerCommandHandler;
+            this._removeCustomerCommandHandler = removeCustomerCommandHandler;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCustomer(int id)
+        {
+            var values = await _getCustomerByIdQueryHandler.Handle(new GetCustomerByIdQuery(id));
+            return Ok(values);
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> CustomerList()
+        {
+            var values = await _getCustomerQueryHandler.Handle();
+            return Ok(values);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> CreateCustomer(CreateCustomerCommand command)
+        {
+            await _createCustomerCommandHandler.Handle(command);
+            return Ok("Müşteri Eklendi");
+        }
+
+        [HttpDelete]
+
+        public async Task<IActionResult> RemoveCustomer(int id)
+        {
+            await _removeCustomerCommandHandler.Handle(new RemoveCustomerCommand(id));
+            return Ok("Müşteri Kaldırıldı");
+        }
+
+        [HttpPut]
+
+        public async Task<IActionResult> UpdateCustomer(UpdateCustomerCommand command)
+        {
+            await _updateCustomerCommandHandler.Handle(command);
+            return Ok("Müşteri Bilgileri Güncellendi");
+        }
+
+
+       
+    }
+}
--- Presentation/ShopCredit.WebApi/Program.cs
+++ Presentation/ShopCredit.WebApi/Program.cs
@@ -1,8 +1,23 @@
+using ShopCredit.Application.CQRS.Handlers;
+using ShopCredit.Application.CQRS.Queries.CustomerQueries;
+using ShopCredit.Application.Interfaces;
+using ShopCredit.Infrastructure.Context;
+using ShopCredit.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddScoped<ShopCreditContext>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
 builder.Services.AddControllers();
+builder.Services.AddScoped<GetCustomerByIdQueryHandler>();
+builder.Services.AddScoped<GetCustomerQueryHandler>();
+builder.Services.AddScoped<CreateCustomerCommandHandler>();
+builder.Services.AddScoped<UpdateCustomerCommandHandler>();
+builder.Services.AddScoped<RemoveCustomerCommandHandler>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
