36ca7f5f41690fe40e3b55cf02dc44dbd0a29805
tolunayz <102868718+tolunayz@users.noreply.github.com>
2024-07-23 13:05:16 +0300

    CQRS

---
+++ ShopCredit.Application/CQRS/Handlers/CustomerHandlers/GetCustomerQueryHandler.cs
@@ -0,0 +1,47 @@
+﻿using ShopCredit.Application.CQRS.Results;
+using ShopCredit.Application.Interfaces;
+using ShopCredit.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopCredit.Application.CQRS.Handlers.CustomerHandlers
+{
+    public class GetCustomerQueryHandler
+    {
+        private readonly IRepository<Customer> _repository;
+
+        public GetCustomerQueryHandler(IRepository<Customer> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetCustomerQueryResult>> Handle()
+        {
+
+            var values = await _repository.GetAllAsync();
+            return values.Select(x => new GetCustomerQueryResult
+            {
+                CustomerID = x.CustomerID,
+                Name = x.Name,
+                Surname = x.Surname,
+                PhoneNumber = x.PhoneNumber,
+                Email= x.Email,
+                Adress= x.Adress
+
+            }
+            ).ToList();
+           
+
+        }
+
+
+
+    }
+
+    
+    
+
+}
---
+++ ShopCredit.Application/CQRS/Queries/CustomerQueries/GetCustomerByIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopCredit.Application.CQRS.Queries.CustomerQueries
+{
+    public class GetCustomerByIdQuery
+    {
+        public GetCustomerByIdQuery(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+
+    }
+}
